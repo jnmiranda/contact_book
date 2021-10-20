d3cf74bdae2e2853be42a9dee9ade65e58c665f6
jnmiranda <jomiranda710@gmail.com>
2021-10-20 23:28:46 +0100

    Add comments to the code to help understand what is being done

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from classes.Person import Person
 
 
 db_exists = exists('contacts.db')
+
+# Open connection to the database
 con = sqlite3.connect('contacts.db')
 cur = con.cursor()
+
+#Checks if the database already exists, if not, creates it
 if db_exists == False:
     cur.execute("CREATE TABLE contacts (name text, phone_number text, email text, address text)")
     cur.execute("INSERT INTO contacts VALUES ('João Miranda', '916660379', 'jomiranda710@gmail.com', 'Rua do Vale do Lobo, Montemor-o-Velho')")
@@ -31,10 +35,14 @@ def show_contact_list():
 def add_contact():
     print('----------Add Contact----------')
     name = input('Name: ')
+
+    # Create the new person object to add to the contact list
     phone_number = input('Phone Number: ')
     email = input('E-Mail: ')
     address = input('Address: ')
     new_contact = Person(name, phone_number, email, address)
+
+    # Add the new person to the contact list
     cur.execute("INSERT INTO contacts VALUES (?, ?, ?, ?)", (new_contact.name, new_contact.phone_number, new_contact.email, new_contact.address))
     con.commit()
 
@@ -45,19 +53,19 @@ def delete_contact():
     print('----------Delete Contact----------')
     contact = input('Which contact do you want to delete? ')
 
+    # Check if the input name exists in the contact list
     cur.execute("SELECT * FROM contacts WHERE name = ?", (contact, ))
     data = cur.fetchall()
+    
+    # If the input name does not exist
     if len(data) == 0:
         print('The contact you are trying to delete does not exist!\n\n')
 
+    # If the input name exists, delete it
     else:
         cur.execute("DELETE FROM contacts WHERE name = ?", (contact,))
         con.commit()
         print(contact, 'was deleted from contact list\n\n')
-
-
-def unavailable_option():
-    return 'Unavailable Option\n\n'
 
 
 def menu():
@@ -85,9 +93,12 @@ def menu():
 
         # Unavailable Option
         else:
-            print(unavailable_option())
+            print('Unavailable Option\n\n')
 
 
 if __name__ == '__main__':
+    # Start running the program by launching the menu
     menu()
+
+    # Close the SQLite3 Database connection
     con.close()
