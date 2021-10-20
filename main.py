0dc4d866f7e79e359d8766c98d03898407cc8c99
jnmiranda <jomiranda710@gmail.com>
2021-10-20 23:17:14 +0100

    Substitutes the contacts list structure for a SQLite3 Database, allowing all the changes to be saved when the program is shutdown

--- main.py
+++ main.py
@@ -4,67 +4,63 @@ from os.path import exists
 from classes.Person import Person
 
 
-def setup(contacts: list):
-    # Check if DB already exists, if not, creates it
-    db_exists = exists('contacts.db')
-    if db_exists == False:
-        print('CREATE DB')
-        con = sqlite3.connect('contacts.db')
-        cur = con.cursor()
-        cur.execute("CREATE TABLE contacts (name text, phone_number text, email text, address text)")
-        cur.execute("INSERT INTO contacts VALUES ('João Miranda', '916660379', 'jomiranda710@gmail.com', 'Rua do Vale do Lobo, Montemor-o-Velho')")
-        cur.execute("INSERT INTO contacts VALUES ('Miguel Torres', '9111111111', 'miki_torres@gmail.com', 'Rua, Santo Varão')")
-        cur.execute("INSERT INTO contacts VALUES ('Lara Trindade', '912222222', 'lara_micaela_trindade2@gmail.com', 'Rua Carminé Miranda, Coimbra')")
-        con.commit()
-    else:
-        print('I EXIST')
+db_exists = exists('contacts.db')
+con = sqlite3.connect('contacts.db')
+cur = con.cursor()
+if db_exists == False:
+    cur.execute("CREATE TABLE contacts (name text, phone_number text, email text, address text)")
+    cur.execute("INSERT INTO contacts VALUES ('João Miranda', '916660379', 'jomiranda710@gmail.com', 'Rua do Vale do Lobo, Montemor-o-Velho')")
+    cur.execute("INSERT INTO contacts VALUES ('Miguel Torres', '9111111111', 'miki_torres@gmail.com', 'Rua, Santo Varão')")
+    cur.execute("INSERT INTO contacts VALUES ('Lara Trindade', '912222222', 'lara_micaela_trindade2@gmail.com', 'Rua Carminé Miranda, Coimbra')")
+    con.commit()
 
 
-'''
-    joao = Person('João Miranda', '916660379', 'jomiranda710@gmail.com', 'Rua do Vale do Logo, Montemor-o-Velho')
-    miguel = Person('Miguel Torres', '9111111111', 'miki_torres@gmail.com', 'Rua, Santo Varão')
-    lara = Person('Lara Trindade', '912222222', 'lara_micaela_trindade2@gmail.com', 'Rua Carminé Miranda, Coimbra')
-
-    contacts.append(joao)
-    contacts.append(miguel)
-    contacts.append(lara)
-'''
-
-def show_contact_list(contacts: list):
+def show_contact_list():
     print('----------Contact List----------')
-    for person in contacts:
-        print('Name: ',person.name)
-        print('Phone Number: ', person.phone_number)
-        print('E-Mail: ', person.email)
-        print('Address: ', person.address)
-        print('\n')
 
+    # Print all contacts
+    for row in cur.execute('SELECT * FROM contacts ORDER BY name'):
+        print('Name:', row[0])
+        print('Phone Number:', row[1])
+        print('E-Mail:', row[2])
+        print('Address:', row[3])
+        print()
+    print()
+        
 
-def add_contact(contacts: list):
+def add_contact():
     print('----------Add Contact----------')
     name = input('Name: ')
     phone_number = input('Phone Number: ')
     email = input('E-Mail: ')
     address = input('Address: ')
     new_contact = Person(name, phone_number, email, address)
-    contacts.append(new_contact)
+    cur.execute("INSERT INTO contacts VALUES (?, ?, ?, ?)", (new_contact.name, new_contact.phone_number, new_contact.email, new_contact.address))
+    con.commit()
+
     print(name, 'added to contact list!\n\n')
 
 
-def delete_contact(contacts: list):
+def delete_contact():
     print('----------Delete Contact----------')
     contact = input('Which contact do you want to delete? ')
-    for item in contacts:
-        if contact == item.name:
-            contacts.remove(item)
-    print(contact, 'was deleted from contact list\n\n')
+
+    cur.execute("SELECT * FROM contacts WHERE name = ?", (contact, ))
+    data = cur.fetchall()
+    if len(data) == 0:
+        print('The contact you are trying to delete does not exist!\n\n')
+
+    else:
+        cur.execute("DELETE FROM contacts WHERE name = ?", (contact,))
+        con.commit()
+        print(contact, 'was deleted from contact list\n\n')
 
 
 def unavailable_option():
     return 'Unavailable Option\n\n'
 
 
-def menu(contacts: list):
+def menu():
     while 1:
         print('----------CONTACTS----------\n1 - Contact List\n2 - Add Contact\n3 - Delete Contact\n0 - Exit')
         # Menu option input
@@ -72,18 +68,15 @@ def menu(contacts: list):
 
         # Contact List
         if option == 1:
-            if len(contacts) == 0:
-                print('Your Contact list is empty')
-            else:
-                show_contact_list(contacts)
+                show_contact_list()
             
         # Add Contact
         elif option == 2:
-            add_contact(contacts)
+            add_contact()
 
         # Delete Contact
         elif option == 3:
-            delete_contact(contacts)
+            delete_contact()
 
         # Exit
         elif option == 0:
@@ -96,8 +89,5 @@ def menu(contacts: list):
 
 
 if __name__ == '__main__':
-    contacts = []
-
-    setup(contacts)
-
-    menu(contacts)
+    menu()
+    con.close()
